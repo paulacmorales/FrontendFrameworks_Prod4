2c3a5e2f6ab3fc2b47a3ad3f671bca3492b94bd0
paulacmorales <paulacmorales@uoc.edu>
2025-05-14 21:00:10 +0200

    Modificaciones varias

--- app/index.tsx
+++ app/index.tsx
@@ -1,1 +1,9 @@
-export { default } from './screens/HomeScreen';
+import { View, Text } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>¡Hola Paula! Tu app funciona 🎉</Text>
+    </View>
+  );
+}
